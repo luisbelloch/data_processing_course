d4604a88be48e7c2676ec63da2ac3377e8fe6a83
Luis Belloch <luisbelloch@gmail.com>
2017-02-01 21:03:43 +0100

    Added color escape codes to explain dirtiness of executions, remove for non-interactive jobs

--- samples/compras_con_mas_de_un_descuento.py
+++ samples/compras_con_mas_de_un_descuento.py
@@ -10,6 +10,8 @@ txt = sc.textFile('data/compras_tiny.csv')
 no_header = txt.filter(lambda s: not s.startswith(item_fields[0]))
 parsed = no_header.map(lambda s: parse_item(s)).cache()
 
+# Brief talk on why color in terminal should not be abused, logs get destroyed
+
 # Primera aproximación
 mas_de_un_cupon = parsed \
     .map(lambda i: (i.tx_id, i.coupon_code)) \
@@ -17,9 +19,9 @@ mas_de_un_cupon = parsed \
     .map(lambda t: (t[0], 1)) \
     .reduceByKey(lambda a, b: a + b) \
     .filter(lambda t: t[1] > 1)
-print("Plan de ejecución (v1):") 
+print("\033[35mPlan de ejecución (v1):\033[0m") 
 print(mas_de_un_cupon.toDebugString().decode('utf-8'))
-print("Con más de un descuento (v1):", mas_de_un_cupon.count())
+print("\033[35mCon más de un descuento (v1):\033[0m", mas_de_un_cupon.count())
 
 # Segunda aproximación, código equivalente
 mas_de_un_cupon2 = parsed \
@@ -27,10 +29,10 @@ mas_de_un_cupon2 = parsed \
     .filter(lambda t: t[1] == 1) \
     .reduceByKey(lambda a, b: a + b) \
     .filter(lambda t: t[1] > 1)
-print("\nPlan de ejecución (v2):") 
+print("\n\033[36mPlan de ejecución (v2):\033[0m") 
 print(mas_de_un_cupon2.toDebugString().decode('utf-8'))
-print("Con más de un descuento (v2):", mas_de_un_cupon2.count())
+print("\033[36mCon más de un descuento (v2):\033[0m", mas_de_un_cupon2.count())
 
 total = parsed.count()
 p_descuentos = mas_de_un_cupon2.count() / float(total)
-print("Porcentaje:", p_descuentos)
+print("\n\x1b[38;5;214mPorcentaje:\x1b[0m", p_descuentos, "\n")
