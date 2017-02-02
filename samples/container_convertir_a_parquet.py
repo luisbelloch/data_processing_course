d34f5c52cd1d84ed10df7ab68495ea6c8b45b722
Luis Belloch <luisbelloch@gmail.com>
2017-02-02 19:45:45 +0100

    Short explanation on types when converting from RDD to DataFrames
    - Removed print statement not needed

--- samples/container_convertir_a_parquet.py
+++ samples/container_convertir_a_parquet.py
@@ -10,8 +10,6 @@ from helpers import *
 sc = SparkContext('local', 'barcos')
 sq = SQLContext(sc)
 
-# Leer archivo como CSV e inferir el esquema
-# Exportar todo como archivo de parquet
 csv_source = sc \
     .textFile('data/containers_tiny.csv') \
     .filter(lambda s: not s.startswith(container_fields[0])) \
@@ -24,14 +22,19 @@ print(csv_source.count())
 # http://stackoverflow.com/a/26180604
 # .map(lambda c: Row(**dict(c.__dict__)))
 
+# Convert RDD to a DataFrame (in scala, DataSet[Row])
+# It will preserve types from the RDD ones. Note it 
+# won't do anything fancy, since the namedtuple types
+# are just strings.
 containerSchema = sq.createDataFrame(csv_source)
 containerSchema.createOrReplaceTempView('container')
+containerSchema.printSchema()
 
 denmark_only = sq.sql("SELECT ship_name FROM container WHERE country = 'DK'")
 print(denmark_only.first())
 
 todo_df = sq.sql("SELECT * FROM container")
-print(todo_df.printSchema())
+todo_df.printSchema()
 
 outpath = 'data/containers_tiny.parquet'
 todo_df.write.mode('overwrite').parquet(outpath)
