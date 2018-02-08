c8bd2601ed1db37061d8b61230d7241917ead1ba
Luis Belloch <luis@luisbelloch.es>
2018-02-09 00:25:02 +0100

    Added code samples to spark folder in jupyter setup

--- infra/pyspark-jupyter/pyspark-jupyter.sh
+++ infra/pyspark-jupyter/pyspark-jupyter.sh
@@ -5,5 +5,8 @@ abs_path() {
     echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 }
 
-docker run -p 8888:8888 -v $(abs_path "../../data"):/opt/notebook/data -ti luisbelloch/pyspark-jupyter
+docker run -p 8888:8888 \
+    -v $(abs_path "../../data"):/opt/notebook/data \
+    -v $(abs_path "../../spark"):/opt/notebook/spark \
+    -ti luisbelloch/pyspark-jupyter
 
