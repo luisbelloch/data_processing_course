63e61ef12d55ab41192ca979b8e3e6f368d40b2e
Luis Belloch <luis@luisbelloch.es>
2020-02-13 22:23:35 +0100

    Autorefresh of live.html, changed file name in server to solve caching issues

--- spark/hello2.py
+++ spark/hello2.py
@@ -5,7 +5,7 @@ rdd = sc.textFile('./data/compras_tiny.csv')
 
 solo_en_euros = rdd.filter(lambda fila: 'EUR' in fila)
 
-print(solo_en_euros.toDebugString())
+print(solo_en_euros.toDebugString().decode('utf-8'))
 print(solo_en_euros.count())
 print(solo_en_euros.take(10))
 
--- spark/live/live.sh
+++ spark/live/live.sh
@@ -5,7 +5,8 @@ fswatch ../live.py | while read -r fpath; do \
     echo -e "\033[0;36mRELOAD\033[0m $fpath $(date +"%H%M%S")"
     echo -e "# $(date +"%H:%M:%S")\n" | cat - ../live.py > live_mod.py
     sed -e '/-python">/r./live_mod.py' live_template.html > live.html
-    gsutil cp live.html gs://bigdata.luisbelloch.es/live.html
+    gsutil -h "Cache-Control:no-cache,max-age=0" \
+        cp live.html gs://bigdata.luisbelloch.es/en_directo.html
 
     # echo -e "# $(date +"%H:%M:%S")\n" | cat - live.py | pygmentize -f html -O full,linenos=1 -o live.html
     # scp live.html root@live.luisbelloch.es:/var/www/html/index.html
