0fe1e841fffe5a9db4225b1bb428506cf617e8c1
Luis Belloch <luis.belloch@flywire.com>
2020-02-21 20:46:01 +0100

    Update assignments spark version

--- assignments/test.sh
+++ assignments/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 readonly WORKDIR=/opt/tests/assignments
-docker run -v $(PWD):${WORKDIR} -w ${WORKDIR} -ti luisbelloch/spark-assignments pytest -v
+docker run -v $(pwd):${WORKDIR} -w ${WORKDIR} -ti luisbelloch/spark-assignments pytest -v
 
