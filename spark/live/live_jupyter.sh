126e6c751b07b64a158020751cc2c1bf7f128ed7
Luis Belloch <luis.belloch@flywire.com>
2020-02-21 15:40:16 +0100

    Add cache control to jupyter notebooks live sync

--- spark/live/live_jupyter.sh
+++ spark/live/live_jupyter.sh
@@ -4,7 +4,8 @@ set -eou pipefail
 fswatch ../live.ipynb | while read -r fpath; do \
   echo -e "\033[0;36mRELOAD\033[0m $fpath $(date +"%H%m%S")"
   jupyter nbconvert ../live.ipynb --to html --output-dir="$(pwd)"
-  gsutil cp live.html gs://bigdata.luisbelloch.es/live.html
+  gsutil -h "Cache-Control:no-cache,max-age=0" \
+    cp live.html gs://bigdata.luisbelloch.es/en_directo.html
   # scp live/live.html root@live.luisbelloch.es:/var/www/html/index.html
 done
 
