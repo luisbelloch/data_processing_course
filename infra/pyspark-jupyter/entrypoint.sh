700dbc77921635f571b232833cdca7e1eba561e0
Luis Belloch <luis@luisbelloch.es>
2021-10-10 22:56:22 +0200

    Add jupyterlab to pyspark-jupyter container, set default

--- infra/pyspark-jupyter/entrypoint.sh
+++ infra/pyspark-jupyter/entrypoint.sh
@@ -2,6 +2,6 @@
 set -eou pipefail
 
 readonly IP=$(awk 'END{print $1}' /etc/hosts)
-export PYSPARK_DRIVER_PYTHON_OPTS="notebook --ip \"${IP}\" --allow-root --port 8888"
+export PYSPARK_DRIVER_PYTHON_OPTS="lab --ip \"${IP}\" --allow-root --port 8888"
 pyspark
 
