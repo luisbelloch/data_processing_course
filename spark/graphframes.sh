451f4080ff982ce28fa285bd2b40fbba8794a730
Luis Belloch <luis@luisbelloch.es>
2021-10-12 21:07:42 +0200

    Upgrade graphframes script to 0.8.1 using Spark 3 as target

--- spark/graphframes.sh
+++ spark/graphframes.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-# 0.5.0 seems to be broken at this point,
-# check this issue https://git.io/vNjRt
-readonly PKG="graphframes:graphframes:0.4.0-spark2.1-s_2.11"
+readonly PKG="graphframes:graphframes:0.8.1-spark3.0-s_2.12"
 if [ $# -eq 0 ]; then
     pyspark --packages $PKG
 else
