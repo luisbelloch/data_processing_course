3c34452322973d58e058328873b80b4dbf4659d7
Luis Belloch <luis@luisbelloch.es>
2021-11-24 17:30:16 +0100

    Wrap gcloud and gsutil in an script

--- spark/live.py
+++ spark/live.py
@@ -3,3 +3,4 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.master("local").appName("SQL").getOrCreate()
 df = spark.read.load('data/containers_tiny.parquet')
 df.select("ship_imo", "container_id", "net_weight").show()
+
--- spark/live/live.sh
+++ spark/live/live.sh
@@ -5,8 +5,8 @@ fswatch ../live.py | while read -r fpath; do \
     echo -e "\033[0;36mRELOAD\033[0m $fpath $(date +"%H%M%S")"
     echo -e "# $(date +"%H:%M:%S")\n" | cat - ../live.py > live_mod.py
     sed -e '/-python">/r./live_mod.py' live_template.html > live.html
-    gsutil -h "Cache-Control:no-cache,max-age=0" \
-        cp live.html gs://bigdata.luisbelloch.es/en_directo.html
+    ./gsutil -h "Cache-Control:no-cache,max-age=0" \
+        cp /tmp/current/live.html gs://bigdata.luisbelloch.es/en_directo.html
 
     # echo -e "# $(date +"%H:%M:%S")\n" | cat - live.py | pygmentize -f html -O full,linenos=1 -o live.html
     # scp live.html root@live.luisbelloch.es:/var/www/html/index.html
