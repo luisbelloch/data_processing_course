fe9fd0b5b895547f25b9ae950d0ac5f091f890a4
Luis Belloch <luis@luisbelloch.es>
2022-11-20 14:28:50 +0100

    Upgrade graphframes to 0.8.2

--- spark/graphframes.sh
+++ spark/graphframes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-readonly PKG="graphframes:graphframes:0.8.1-spark3.0-s_2.12"
+readonly PKG="graphframes:graphframes:0.8.2-spark3.2-s_2.12"
 if [ $# -eq 0 ]; then
     pyspark --packages $PKG
 else
